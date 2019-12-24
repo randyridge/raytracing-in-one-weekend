48a080f842081b8961b58c6e56e72f3030d3ba0b
Randy Ridge <randyridge@gmail.com>
2019-12-24 12:51:12 -0500

    ported chapter 02 (#4)

--- RayTracing/Program.cs
+++ RayTracing/Program.cs
@@ -1,9 +1,22 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 
 namespace RayTracing {
     internal static class Program {
-        private static Task Main() {
-            return Task.CompletedTask;
+        private static void Main() {
+            const int nx = 200;
+            const int ny = 100;
+            Console.Write($"P3\n {nx} {ny}\n255\n");
+            for(var j = ny - 1; j >= 0; j--) {
+                for(var i = 0; i < nx; i++) {
+                    var r = i / (float) nx;
+                    var g = j / (float) ny;
+                    var b = 0.2f;
+                    var ir = (int) (255.99f * r);
+                    var ig = (int) (255.99f * g);
+                    var ib = (int) (255.99f * b);
+                    Console.WriteLine($"{ir} {ig} {ib}\n");
+                }
+            }
         }
     }
 }
